dd321276e22e370b005b13a3c2bedc76094d09aa
yashdamani <yashdamani1997@gmail.com>
2018-08-06 11:00:49 +0530

    added property

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var strings = require('./stringexample');
 var functions = require('./functionexample');
 var objects = require('./objects');
 var classes = require('./class');
+var prop = require('./property');
 
 var obj1 = new classes("abc@gmail.com", "yash");
 
@@ -10,6 +11,7 @@ console.log(functions.one("Hello world from function"));//function one
 console.log(functions.two("Hello world from another function"));//function two
 console.log(objects.name + " " +objects.email);//objects
 console.log(obj1.email + " " + obj1.name);//classes
+console.log(prop.SimpleMessage);
 
 
 
---
+++ property.js
@@ -0,0 +1,1 @@
+module.exports.SimpleMessage = "hello world property";
